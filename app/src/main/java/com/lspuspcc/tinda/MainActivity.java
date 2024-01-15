7cced962cb074bb591f5113d1a3986a95357bb17
Jhondale Llaguno <jhondalellaguno@gmail.com>
2024-01-15 12:47:51 +0800

    improve bottom navigations

--- app/src/main/java/com/lspuspcc/tinda/MainActivity.java
+++ app/src/main/java/com/lspuspcc/tinda/MainActivity.java
@@ -20,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Hide action bar
+        if (getSupportActionBar() != null)
+            getSupportActionBar().hide();
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
