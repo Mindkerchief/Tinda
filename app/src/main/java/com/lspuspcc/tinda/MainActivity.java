7f7f56ac683e512a7098829ef0d1e3c8fcf2bd8c
Jhondale Llaguno <jhondalellaguno@gmail.com>
2024-01-17 17:09:26 +0800

    Some minor fixes and improvement

--- app/src/main/java/com/lspuspcc/tinda/MainActivity.java
+++ app/src/main/java/com/lspuspcc/tinda/MainActivity.java
@@ -7,7 +7,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.lspuspcc.tinda.databinding.ActivityMainBinding;
@@ -20,22 +19,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Hide action bar
-        if (getSupportActionBar() != null)
-            getSupportActionBar().hide();
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_basket, R.id.navigation_account)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // Hide the action bar
+        if (getSupportActionBar() != null)
+            getSupportActionBar().hide();
     }
 
 }
