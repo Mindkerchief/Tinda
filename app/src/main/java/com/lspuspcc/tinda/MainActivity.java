123738f451f83349990b7edc5d6b993c5970bfa3
Jhondale Llaguno <jhondalellaguno@gmail.com>
2024-01-18 13:03:10 +0800

    Added most of the necessary activity found in home fragment.

--- app/src/main/java/com/lspuspcc/tinda/MainActivity.java
+++ app/src/main/java/com/lspuspcc/tinda/MainActivity.java
@@ -22,17 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
         // Hide the action bar
         if (getSupportActionBar() != null)
             getSupportActionBar().hide();
+
+        // Prevents night mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 
 }
