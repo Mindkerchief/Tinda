bf9a8656e35f11e55eb29101989c039d493b9116
Jhondale Llaguno <jhondalellaguno@gmail.com>
2024-01-18 14:23:39 +0800

    Minor fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-view-model-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment:2.7.6")
     implementation("androidx.navigation:navigation-ui:2.7.6")
     testImplementation("junit:junit:4.13.2")
