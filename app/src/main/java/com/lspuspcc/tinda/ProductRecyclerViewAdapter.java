18b8ebe02de602b74c42dbb7edd9b588aade9167
Jhondale Llaguno <jhondalellaguno@gmail.com>
2024-01-19 13:24:21 +0800

    Setup the Product and Store base ViewAdapter for recycler view

---
+++ app/src/main/java/com/lspuspcc/tinda/ProductRecyclerViewAdapter.java
@@ -0,0 +1,34 @@
+package com.lspuspcc.tinda;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProductRecyclerViewAdapter extends RecyclerView.Adapter<StoreRecyclerViewAdapter.MyViewHolder> {
+    @NonNull
+    @Override
+    public StoreRecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate the layout
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StoreRecyclerViewAdapter.MyViewHolder holder, int position) {
+        // Assign values in the views
+    }
+
+    @Override
+    public int getItemCount() {
+        // counts number of items to be displayed
+        return 0;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // gets the views from recycler view rows
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
