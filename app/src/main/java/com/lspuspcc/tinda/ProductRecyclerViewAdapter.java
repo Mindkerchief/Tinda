4fbb2aa36ded9bb1150d89b320a61f9830c49b3d
Jhondale Llaguno <jhondalellaguno@gmail.com>
2024-01-19 13:51:01 +0800

    Finalized the Product and Store ViewAdapter for recycler view

--- app/src/main/java/com/lspuspcc/tinda/ProductRecyclerViewAdapter.java
+++ app/src/main/java/com/lspuspcc/tinda/ProductRecyclerViewAdapter.java
@@ -1,34 +1,61 @@
 package com.lspuspcc.tinda;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ProductRecyclerViewAdapter extends RecyclerView.Adapter<StoreRecyclerViewAdapter.MyViewHolder> {
+import java.util.ArrayList;
+
+public class ProductRecyclerViewAdapter extends RecyclerView.Adapter<ProductRecyclerViewAdapter.MyViewHolder> {
+    Context context;
+    ArrayList<ProductModel> productModels;
+
+    public ProductRecyclerViewAdapter(Context context, ArrayList<ProductModel> productModels) {
+        this.context = context;
+        this.productModels = productModels;
+    }
+
     @NonNull
     @Override
-    public StoreRecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ProductRecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // Inflate the layout
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.card_product, parent, false);
+        return new ProductRecyclerViewAdapter.MyViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StoreRecyclerViewAdapter.MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ProductRecyclerViewAdapter.MyViewHolder holder, int position) {
         // Assign values in the views
+        holder.imageVProductImage.setImageResource(productModels.get(position).getProductImage());
+        holder.textVProductName.setText(productModels.get(position).getProductName());
+        holder.textVProductStore.setText(productModels.get(position).getProductStore());
+        holder.textVProductPrice.setText(productModels.get(position).getProductPrice());
     }
 
     @Override
     public int getItemCount() {
         // counts number of items to be displayed
-        return 0;
+        return productModels.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // gets the views from recycler view rows
+        ImageView imageVProductImage;
+        TextView textVProductName, textVProductStore, textVProductPrice;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imageVProductImage = itemView.findViewById(R.id.imageVProductImage);
+            textVProductName = itemView.findViewById(R.id.textVProductName);
+            textVProductStore = itemView.findViewById(R.id.textVProductStore);
+            textVProductPrice = itemView.findViewById(R.id.textVProductPrice);
         }
     }
 }
--- app/src/main/java/com/lspuspcc/tinda/StoreRecyclerViewAdapter.java
+++ app/src/main/java/com/lspuspcc/tinda/StoreRecyclerViewAdapter.java
@@ -1,35 +1,61 @@
 package com.lspuspcc.tinda;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class StoreRecyclerViewAdapter extends RecyclerView.Adapter<StoreRecyclerViewAdapter.MyViewHolder> {
+    Context context;
+    ArrayList<StoreModel> storeModels;
+
+    public StoreRecyclerViewAdapter(Context context, ArrayList<StoreModel> storeModels) {
+        this.context = context;
+        this.storeModels = storeModels;
+    }
 
     @NonNull
     @Override
     public StoreRecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // Inflate the layout
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.card_store, parent, false);
+        return new StoreRecyclerViewAdapter.MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull StoreRecyclerViewAdapter.MyViewHolder holder, int position) {
         // Assign values in the views
+        holder.imageVStoreImage.setImageResource(storeModels.get(position).getStoreImage());
+        holder.textVStoreName.setText(storeModels.get(position).getStoreName());
+        holder.textVStoreAddress.setText(storeModels.get(position).getStoreAddress());
+        holder.textVStoreCategory.setText(storeModels.get(position).getStoreCategory());
     }
 
     @Override
     public int getItemCount() {
         // counts number of items to be displayed
-        return 0;
+        return storeModels.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // gets the views from recycler view rows
+        ImageView imageVStoreImage;
+        TextView textVStoreName, textVStoreAddress, textVStoreCategory;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imageVStoreImage = itemView.findViewById(R.id.imageVStoreImage);
+            textVStoreName = itemView.findViewById(R.id.textVStoreName);
+            textVStoreAddress = itemView.findViewById(R.id.textVStoreAddress);
+            textVStoreCategory = itemView.findViewById(R.id.textVStoreCategory);
         }
     }
 }
