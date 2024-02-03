3078d6a578b66bc88d07782864b8611ac7e94f50
Jhondale Llaguno <jhondalellaguno@gmail.com>
2024-02-03 20:01:07 +0800

    Added a temporary layout and functionality for Deal Activity.

--- app/src/main/java/com/lspuspcc/tinda/ui/deal/DealActivity.java
+++ app/src/main/java/com/lspuspcc/tinda/ui/deal/DealActivity.java
@@ -4,8 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.lspuspcc.tinda.R;
 import com.lspuspcc.tinda.databinding.ActivityDealBinding;
+import com.lspuspcc.tinda.ui.searchbar.SearchBarViewModel;
+import com.lspuspcc.tinda.ui.searchbar.SearchResultViewModel;
 
 public class DealActivity extends AppCompatActivity {
     private ActivityDealBinding mDealBinding;
@@ -15,5 +16,16 @@ public class DealActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mDealBinding = ActivityDealBinding.inflate(getLayoutInflater());
         setContentView(mDealBinding.getRoot());
+
+        SearchBarViewModel searchBarViewModel = new SearchBarViewModel(this,
+                mDealBinding.includeLSearchBar, "deal");
+        new SearchResultViewModel(this, searchBarViewModel,
+                mDealBinding.includeLSearchResult, mDealBinding.nestedSVSearchResults, "deal");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDealBinding = null;
     }
 }
