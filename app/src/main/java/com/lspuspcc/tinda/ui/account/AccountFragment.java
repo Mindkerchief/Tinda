803f54ce010d8abcb25ee858f07ffc844a0abd27
Jhondale Llaguno <jhondalellaguno@gmail.com>
2024-02-06 16:23:58 +0800

    Added a basic UI layout for Account Tab

--- app/src/main/java/com/lspuspcc/tinda/ui/account/AccountFragment.java
+++ app/src/main/java/com/lspuspcc/tinda/ui/account/AccountFragment.java
@@ -4,9 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -14,24 +14,23 @@ import com.lspuspcc.tinda.databinding.FragmentAccountBinding;
 
 public class AccountFragment extends Fragment {
 
-    private FragmentAccountBinding binding;
+    private FragmentAccountBinding AccountBinding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        AccountViewModel accountViewModel =
-                new ViewModelProvider(this).get(AccountViewModel.class);
+        AccountViewModel accountViewModel = new ViewModelProvider(this).get(AccountViewModel.class);
+        AccountBinding = FragmentAccountBinding.inflate(inflater, container, false);
+        return AccountBinding.getRoot();
+    }
 
-        binding = FragmentAccountBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textAccount;
-        accountViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+        AccountBinding = null;
     }
 }
